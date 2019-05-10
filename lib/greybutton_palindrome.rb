3942facc340e9cc8a93f6be01779948ad07cab0b
greybutton <greybutton@yandex.com>
2019-05-10 20:00:35 +0800

    init

---
+++ lib/greybutton_palindrome.rb
@@ -0,0 +1,6 @@
+require "greybutton_palindrome/version"
+
+module GreybuttonPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/greybutton_palindrome/version.rb
@@ -0,0 +1,3 @@
+module GreybuttonPalindrome
+  VERSION = "0.1.0"
+end
