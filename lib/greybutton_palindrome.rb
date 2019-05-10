608f26103d6efad78db92781d4cfb760b0d91993
greybutton <greybutton@yandex.com>
2019-05-10 20:52:16 +0800

    done 8.4

--- lib/greybutton_palindrome.rb
+++ lib/greybutton_palindrome.rb
@@ -8,12 +8,19 @@ class String
 
   # Returns the letters in the string.
   def letters
+    the_letters = []
+    for i in 0..self.length - 1 do
+      if self[i].match(/[a-zA-Z]/)
+        the_letters << self[i]
+      end
+    end
+    the_letters.join
   end
 
   private
 
   # Returns content for palindrome testing.
   def processed_content
-    downcase
+    letters.downcase
   end
 end
